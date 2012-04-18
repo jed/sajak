760374e32f160608299487db181d3b6a3e206b9f
Jed Schmidt <tr@nslator.jp>
2012-04-18 18:53:04 +0900

    first round of tests

--- test/api.js
+++ test/api.js
@@ -4,17 +4,40 @@ function User(attrs) {
   this.id = attrs.id
 }
 
-User.prototype.authorize = function(user, method, cb) {
-  cb()
+User.prototype.authorize = function(user, method, next) {
+  if (this.id != user.id) {
+    return next(new Error("Users cannot access other user profiles."))
+  }
+
+  next()
 }
 
-User.prototype.authenticate = function(auth, cb) {
-  this.auth = auth
-  cb()
+User.prototype.authenticate = function(auth, next) {
+  if (!auth.scheme) {
+    return next(new Error("Authentication required."))
+  }
+
+  if (auth.scheme.toLowerCase() != "password") {
+    return next(new Error("Only basic authentication accepted"))
+  }
+
+  if (auth.token.password != "pass" || auth.token.username != "sajak") {
+    return next(new Error("Incorrect username/password"))
+  }
+
+  this.id = auth.token.username
+
+  next()
 }
 
 User.prototype.fetch = function(cb) {
   cb(null, this)
 }
 
-module.exports = sajak([User])
+function Note() {}
+
+Note.prototype.fetch = function(cb) {
+  cb(null, this)
+}
+
+module.exports = sajak([User, Note])
--- test/test.js
+++ test/test.js
@@ -6,26 +6,111 @@ var http    = require("http")
 
 describe("sajak", function() {
   before(function(done) {
-    server.listen(3000, done)
+    server.listen(8000, done)
   })
 
-  describe("headers", function() {
+  describe("json", function() {
+    it("should have content-length", function(done) {
+      request("http://sajak:pass@localhost:8000/", function(err, res, body) {
+        res.headers.should.have.property("content-length")
+        done()
+      })
+    })
 
+    it("should have content-type 'application/json'", function(done) {
+      request("http://sajak:pass@localhost:8000/", function(err, res, body) {
+        res.headers.should.have.property("content-type", "application/json; charset=utf-8")
+        done()
+      })
+    })
+
+    it("should be valid JSON", function(done) {
+      request("http://sajak:pass@localhost:8000/", function(err, res, body) {
+        (function(){ JSON.parse(body) }).should.not.throw()
+        done()
+      })
+    })
+  })
+
+  describe("routing", function() {
+    it("should return 200 for root", function(done) {
+      request("http://sajak:pass@localhost:8000/", function(err, res, body) {
+        res.statusCode.should.eql(200)
+        done()
+      })
+    })
+
+    it("should return 200 for valid model", function(done) {
+      request("http://sajak:pass@localhost:8000/notes", function(err, res, body) {
+        res.statusCode.should.eql(200)
+        done()
+      })
+    })
+
+    it("should return 404 for invalid model", function(done) {
+      request("http://sajak:pass@localhost:8000/unknowkn_model", function(err, res, body) {
+        res.statusCode.should.eql(404)
+        done()
+      })
+    })
+
+    it("should return 405 for invalid action", function(done) {
+      request.del("http://sajak:pass@localhost:8000/notes/123", function(err, res, body) {
+        res.statusCode.should.eql(405)
+        done()
+      })
+    })
   })
 
   describe("authentication", function() {
+    it("should return 200 for valid password", function(done) {
+      request("http://sajak:pass@localhost:8000/", function(err, res, body) {
+        res.statusCode.should.eql(200)
+        done()
+      })
+    })
 
+    it("should return 401 for missing auth", function(done) {
+      request("http://localhost:8000/", function(err, res, body) {
+        res.statusCode.should.eql(401)
+        done()
+      })
+    })
+
+    it("should return 401 for incorrect password", function(done) {
+      request("http://sajak:wrongpass@localhost:8000/", function(err, res, body) {
+        res.statusCode.should.eql(401)
+        done()
+      })
+    })
+
+    it("should return 401 for incorrect user", function(done) {
+      request("http://notsajak:pass@localhost:8000/", function(err, res, body) {
+        res.statusCode.should.eql(401)
+        done()
+      })
+    })
   })
 
   describe("authorization", function() {
+    it("should return 200 for authorized resource", function(done) {
+      request("http://sajak:pass@localhost:8000/users/sajak", function(err, res, body) {
+        res.statusCode.should.eql(200)
+        done()
+      })
+    })
+
+    it("should return 403 for unauthorized resource", function(done) {
+      request("http://sajak:pass@localhost:8000/users/notsajak", function(err, res, body) {
+        res.statusCode.should.eql(403)
+        done()
+      })
+    })
 
   })
 
-  describe("response", function() {
-
-  })
-
-  before(function() {
+  after(function(done) {
     server.close()
+    done()
   })
 })
